022382528fb9d20a91142ac0ab7bf88e6844f122
Weliton Martins <weliton.martins@yaman.com>
2020-08-25 23:16:39 -0300

    estrutura inicial

---
+++ src/main/java/com/github/welitonamartins/msvrangoprodutos/MsvRangoProdutosApplication.java
@@ -0,0 +1,13 @@
+package com.github.welitonamartins.msvrangoprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvRangoProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvRangoProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/welitonamartins/msvrangoprodutos/MsvRangoProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.welitonamartins.msvrangoprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvRangoProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
